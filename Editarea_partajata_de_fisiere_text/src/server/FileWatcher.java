e6a69b2157713788f1b61ef0bbc8923c7046b111
Ana-Miruna Grigore <145209763+mirunagrigore1234@users.noreply.github.com>
2026-04-21 12:54:07 +0300

    Update log messages in FileWatcher for English

--- Editarea_partajata_de_fisiere_text/src/server/FileWatcher.java
+++ Editarea_partajata_de_fisiere_text/src/server/FileWatcher.java
@@ -8,7 +8,6 @@ public class FileWatcher implements Runnable {
 
     public FileWatcher(String dir) {
     	this.folder = Paths.get(System.getProperty("user.dir"), "server_files");
-        System.out.println("Watcher pornit pe: " + folder.toAbsolutePath());
     }
 
     @Override
@@ -35,12 +34,12 @@ public class FileWatcher implements Runnable {
                     if (!file.endsWith(".txt")) continue;
 
                     if (kind == StandardWatchEventKinds.ENTRY_CREATE) {
-                        System.out.println("Fisier adaugat: " + file);
+                        System.out.println("File added: " + file);
                         ServerMain.broadcast("INFO: FILE_ADDED " + file, null);
                     }
 
                     if (kind == StandardWatchEventKinds.ENTRY_DELETE) {
-                        System.out.println("Fisier sters: " + file);
+                        System.out.println("File deleted: " + file);
                         ServerMain.broadcast("INFO: FILE_REMOVED " + file, null);
                     }
                 }
